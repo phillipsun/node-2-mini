f60ea445ec97b5d91ada126bb4b8339c37ca515b
Phillip Sun <psun.04@gmail.com>
2018-05-07 22:27:46 -0500

    finished node 2 mini

--- db/new_planes.sql
+++ db/new_planes.sql
@@ -3,3 +3,4 @@ INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('717', 10);
 INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('727', 20);
 INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('737', 30);
 INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('757', 500);
+INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('555', 1000);
